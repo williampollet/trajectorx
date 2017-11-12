cf0e7512ab72637f9ec41976cdcae0a08a314ef2
William <polletwilliam@gmail.com>
2017-11-12 18:24:46 +0100

    adding bundler

---
+++ app/javascript/bundles/Planets/components/Earth.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+export default class Earth extends React.Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired, // this is passed from the Rails view
+  };
+
+  /**
+   * @param props - Comes from your rails view.
+   */
+
+   constructor(props) {
+     super(props);
+
+     // How to set initial state in ES6 class syntax
+     // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
+     this.state = { name: this.props.name };
+   }
+}
